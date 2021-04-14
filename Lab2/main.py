7e9d36dfb47e8af523623d3c5922f0c8cceffae0
alexeymoskvinov <alexey.moskvinov@yandex.ru>
2021-04-14 17:35:22 +0300

    2

--- Lab2/main.py
+++ Lab2/main.py
@@ -2,23 +2,15 @@
 import ConjugateGradientMethod
 import numpy as np
 from scipy.sparse.linalg import cg
-import time
+
 
 n = 1000
 P = np.random.normal(size=[n, n])
 A = np.dot(P.T, P)
 b = np.ones(n)
 
-t1 = time.time()
-print('start')
-x = ConjugateGradientMethod.conjugate_grad(A, b)
-t2 = time.time()
-print(t2 - t1)
-x2 = np.linalg.solve(A, b)
-t3 = time.time()
-print(t3 - t2)
-x3 = cg(A, b)
-t4 = time.time()
-print(t4 - t3)
 
+x = ConjugateGradientMethod.conjugate_grad(A, b)
+x2 = np.linalg.solve(A, b)
+x3 = cg(A, b)
 print(np.dot(A, x) - b)
