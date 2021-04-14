317c6785cc72f12dd48c55a0562c9da6596389a4
alexeymoskvinov <alexey.moskvinov@yandex.ru>
2021-04-14 19:51:10 +0300

    3

--- Lab2/main.py
+++ Lab2/main.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.sparse.linalg import cg
 
 
-n = 1000
+n = 2
 P = np.random.normal(size=[n, n])
 A = np.dot(P.T, P)
 b = np.ones(n)
@@ -13,4 +13,5 @@ b = np.ones(n)
 x = ConjugateGradientMethod.conjugate_grad(A, b)
 x2 = np.linalg.solve(A, b)
 x3 = cg(A, b)
-print(np.dot(A, x) - b)
+#print(np.dot(A, x) - b)
+print(x)
