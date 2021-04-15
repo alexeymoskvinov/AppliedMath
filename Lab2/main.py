bf53f8bff895c76145b6f538522c723698bb475c
alexeymoskvinov <alexey.moskvinov@yandex.ru>
2021-04-15 15:11:04 +0300

    4

--- Lab2/main.py
+++ Lab2/main.py
@@ -2,16 +2,27 @@
 import ConjugateGradientMethod
 import numpy as np
 from scipy.sparse.linalg import cg
-
+import scipy.optimize as scm
+from scipy import optimize
 
 n = 2
 P = np.random.normal(size=[n, n])
 A = np.dot(P.T, P)
+print(A)
 b = np.ones(n)
-
+print(b)
 
 x = ConjugateGradientMethod.conjugate_grad(A, b)
-x2 = np.linalg.solve(A, b)
+#x2 = np.linalg.solve(A, b)
 x3 = cg(A, b)
 #print(np.dot(A, x) - b)
 print(x)
+#print(x2)
+print(x3)
+
+def f(x):
+    return x**2
+
+
+#minimum = optimize.fmin_powell(f, -1)
+#print(minimum)
